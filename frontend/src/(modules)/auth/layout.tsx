30a542293b139a629603c9faec82f6274f361e0b
david-7279 <david.dev7279@outlook.com>
2026-06-02 18:43:11 +0100

    feat: add authentication routes and AuthLayout component

---
+++ frontend/src/(modules)/auth/layout.tsx
@@ -0,0 +1,8 @@
+import {Outlet} from "react-router";
+
+const AuthLayout = () => {
+    return (
+        <Outlet />
+    )
+}
+export default AuthLayout
--- frontend/src/router/app-router.tsx
+++ frontend/src/router/app-router.tsx
@@ -1,10 +1,18 @@
 import {Route, Routes} from "react-router";
 import Page from "../page.tsx";
+import AuthLayout from "../(modules)/auth/layout.tsx";
+import LoginPage from "../(modules)/auth/login/page.tsx";
+import RegisterPage from "../(modules)/auth/register/page.tsx";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Page/>}/>
+
+            <Route path="/auth" element={<AuthLayout />}>
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<RegisterPage />} />
+            </Route>
         </Routes>
     )
 }
