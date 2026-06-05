ebc68976ad60f084855c55bb9c44b7cd5bd46a5e
david-7279 <david.dev7279@outlook.com>
2026-06-05 20:53:49 +0100

    feat: add updateBook and deleteBook controllers for book management

--- backend/src/modules/book/book.controller.js
+++ backend/src/modules/book/book.controller.js
@@ -1,5 +1,6 @@
 import * as bookService from "./book.service.js";
 import {toBookCreate} from "./dto/book.create.dto.js";
+import {toBookUpdate} from "./dto/book.update.dto.js";
 
 export const createBook = async (req, res, next) => {
     try {
@@ -7,6 +8,34 @@ export const createBook = async (req, res, next) => {
         const result = await bookService.createBook(bookData);
         res.status(201).json({
             message: "Book created successfully",
+            book: result,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export const updateBook = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const bookData = toBookUpdate(req.body);
+        const result = await bookService.update(id, bookData);
+        res.status(200).json({
+            message: "Book updated successfully",
+            book: result,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteBook = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const result = await bookService.deleteBook(id);
+        res.status(200).json({
+            message: "Book deleted successfully",
             book: result,
         });
     } catch (error) {
