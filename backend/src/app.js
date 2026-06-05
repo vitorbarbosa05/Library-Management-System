332053af20763d8402fec810f03b8d33dd3577b1
david-7279 <david.dev7279@outlook.com>
2026-06-05 20:58:03 +0100

    feat: add book router to handle book-related API requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import actuatorRouter from "./actuator/index.js";
 import {errorMiddleware} from "./shared/errors/error.middleware.js";
 import authRouter from "./modules/auth/auth.routes.js";
+import bookRouter from "./modules/book/book.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1/actuator', actuatorRouter);
-app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/book', bookRouter);
 
 app.use(errorMiddleware);
 
