c65c0e1561656079f4bd127146b79a11edf91cad
david-7279 <david.dev7279@outlook.com>
2026-06-06 16:00:25 +0100

    feat: enhance authentication pages with layout improvements and registration form

--- frontend/src/(modules)/auth/ui/login/page.tsx
+++ frontend/src/(modules)/auth/ui/login/page.tsx
@@ -1,9 +1,39 @@
 import ModuleWrapper from "../../../../components/shared/module-wrapper.tsx";
+import {Separator} from "@/src/components/ui/separator.tsx";
+import LinkAnimated from "@/src/components/shared/link-animated.tsx";
+import {Path} from "@/src/router/paths.ts";
+import {LibraryBigIcon} from "lucide-react";
 
 const LoginPage = () => {
     return (
-        <ModuleWrapper>
-            LoginPage
+        <ModuleWrapper classname="flex flex-col justify-center items-center mx-auto h-screen space-y-2">
+            <header className="space-y-1.5">
+                <LibraryBigIcon/>
+                <h3 className="scroll-m-20 text-2xl font-medium tracking-tight">
+                    Library
+                </h3>
+                <p className="text-sm text-muted-foreground">
+                    Sign in to manage your library network
+                </p>
+            </header>
+
+            <main>
+
+            </main>
+
+            <footer className="flex gap-2 text-xs font-medium text-muted-foreground">
+                <LinkAnimated
+                    to={Path.privacyPolicy}
+                    text="Privacy Policy"
+                />
+
+                <Separator orientation="vertical"/>
+
+                <LinkAnimated
+                    to={Path.termsOfService}
+                    text="Terms of Service"
+                />
+            </footer>
         </ModuleWrapper>
     )
 }
--- frontend/src/(modules)/auth/ui/register/page.tsx
+++ frontend/src/(modules)/auth/ui/register/page.tsx
@@ -1,9 +1,40 @@
 import ModuleWrapper from "../../../../components/shared/module-wrapper.tsx";
+import {LibraryBigIcon} from "lucide-react";
+import LinkAnimated from "@/src/components/shared/link-animated.tsx";
+import {Path} from "@/src/router/paths.ts";
+import {Separator} from "@/src/components/ui/separator.tsx";
+import FormRegister from "@/src/(modules)/auth/ui/forms/FormRegister.tsx";
 
 const RegisterPage = () => {
     return (
-        <ModuleWrapper>
-            RegisterPage
+        <ModuleWrapper classname="flex flex-col justify-center items-center mx-auto h-screen space-y-2">
+            <header className="space-y-1.5">
+                <LibraryBigIcon/>
+                <h3 className="scroll-m-20 text-2xl font-medium tracking-tight">
+                    Library
+                </h3>
+                <p className="text-sm text-muted-foreground">
+                    Sign up to manage your library network
+                </p>
+            </header>
+
+            <main className="max-w-xs w-full">
+                <FormRegister/>
+            </main>
+
+            <footer className="flex gap-2 text-xs font-medium text-muted-foreground">
+                <LinkAnimated
+                    to={Path.privacyPolicy}
+                    text="Privacy Policy"
+                />
+
+                <Separator orientation="vertical"/>
+
+                <LinkAnimated
+                    to={Path.termsOfService}
+                    text="Terms of Service"
+                />
+            </footer>
         </ModuleWrapper>
     )
 }
