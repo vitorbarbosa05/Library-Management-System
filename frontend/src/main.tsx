31dbe752495eb4ca92dcd9b0f5935893f04acb88
david-7279 <david.dev7279@outlook.com>
2026-06-06 16:00:15 +0100

    feat: add Toaster component for notifications and define application paths

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import {createRoot} from "react-dom/client"
 import {BrowserRouter} from "react-router";
 import AppRouter from "@/src/router/app-router.tsx";
+import { Toaster } from "@/src/components/ui/sonner"
 import "./globals.css"
 
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <AppRouter/>
+        <Toaster />
     </BrowserRouter>,
 )
---
+++ frontend/src/router/paths.ts
@@ -0,0 +1,21 @@
+export const Path = {
+    // AUTH
+    login: "/login",
+    register: "/register",
+
+    termsOfService: "/terms",
+    privacyPolicy: "/privacy",
+
+    // BACKOFFICE
+    adminDashboard: "/admin/dashboard",
+
+    // MODULES
+    // OVERVIEW
+    dashboard: "/dashboard",
+
+    // FEATURES
+
+    // SYSTEM
+    settings: "/settings",
+    profile: "/profile",
+};
