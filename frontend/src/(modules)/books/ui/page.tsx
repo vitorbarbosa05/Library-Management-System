55755f46d3e980be31cfc97cbfe9906d04a94e87
david-7279 <david.dev7279@outlook.com>
2026-07-02 22:59:18 +0100

    feat: create BooksPage component for managing book collection

---
+++ frontend/src/(modules)/books/ui/page.tsx
@@ -0,0 +1,15 @@
+import ModuleWrapper from "@/src/components/shared/module-wrapper.tsx";
+import ModuleTitle from "@/src/components/shared/module-title.tsx";
+
+const BooksPage = () => {
+    return (
+        <ModuleWrapper>
+            <ModuleTitle
+                title="Books"
+                description="Manage your collection, track availability and update metadata"
+            />
+            BooksPage
+        </ModuleWrapper>
+    )
+}
+export default BooksPage
